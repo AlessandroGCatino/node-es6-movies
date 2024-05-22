cc218a18905c2fcdfbe636b9b57cfbbbf65518de
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-05-22 15:32:14 +0200

    creazione getter e setter + map

--- app.js
+++ app.js
@@ -47,6 +47,14 @@ const library = [
         type: "movie",
         seasons: null 
     },
+    {
+        title: "Shogun",
+        year: 2024,
+        genre: ["Adventure", "Drama", "History"],
+        rating: 8.1,
+        type: "serie tv",
+        seasons: 1
+      }
   ];
   
 class Movie {
@@ -58,14 +66,51 @@ class Movie {
     #type;
 
     constructor(title, year, genre, rating, type){
-        this.title = title
-        this.title = year
-        this.title = genre
-        this.title = rating
-        this.title = type
+        this.#title = title;
+        this.#year = year;
+        this.#genre = genre;
+        this.#rating = rating;
+        this.#type = type;
     }
 
-    
+    setTitle(title){
+        this.#title = title
+    }
+    getTitle(){
+        return this.#title
+    }
+
+    setYear(year){
+        this.#year = year
+    }
+    getYear(){
+        return this.#year
+    }
+
+    setGenres(genres){
+        this.#genre = genres
+    }
+    getGenres(){
+        return this.#genre.join(", ")
+    }
+
+    setRating(rating){
+        this.#rating = rating
+    }
+    getRating(){
+        return this.#rating
+    }
+
+    setType(type){
+        this.#type = type
+    }
+    getType(){
+        return this.#type
+    }
+
+    toString(){
+        return `${this.getTitle()} è un ${this.getType()} di genere ${this.getGenres()}. É stato rilasciato nel ${this.getYear()} ed ha un voto di ${this.getRating()}`
+    }
 }
 
 class TvSerie extends Movie{
@@ -74,7 +119,28 @@ class TvSerie extends Movie{
 
     constructor(title, year, genre, rating, type, seasons){
         super(title, year, genre, rating, type);
-        this.seasons = seasons;
+        this.#seasons = seasons;
+    }
+
+    setSeasons(seasons){
+        this.#seasons = seasons
+    }
+    getSeasons(){
+        return this.#seasons
+    }
+
+    toString(){
+        return `${this.getTitle()} è una ${this.getType()} di genere ${this.getGenres()}. La prima stagione è stata rilasciata nel ${this.getYear()}, conta ${this.getSeasons()} ${(this.getSeasons()===1 ? "stagione" : "stagioni")} ed ha un voto di ${this.getRating()}`
     }
 
 }
+
+const arrayMoviesTv = library.map((element) => {
+    if(element.type === "movie"){
+        return new Movie(element.title, element.year, element.genre, element.rating, element.type)
+    } else {
+        return new TvSerie(element.title, element.year, element.genre, element.rating, element.type, element.seasons)
+    }
+})
+
+// output.forEach( element => console.log(element.toString()))
